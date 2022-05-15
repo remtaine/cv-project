700abf3d16e81d21b1f90c3d1122334a6b26adb4
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-15 16:50:45 +0800

    Add generated file for React-PDF
    placeholder for now

---
+++ src/components/GeneratedFile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function GeneratedFile(props) {
+  return (
+    <div>GeneratedFile</div>
+  )
+}
+
+GeneratedFile.propTypes = {}
+
+export default GeneratedFile
