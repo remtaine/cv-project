431131fac830fe317b8325206ae68d98f6f199f9
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-15 16:51:01 +0800

    Add name and link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className='flex justify-center '>
+    <header className='flex justify-center flex-col'>
         <h1 className="text-3xl font-bold text-center">CV Creator</h1>
+        <span className="mt-1 mx-auto left-0 right-0 text-center text-xs font-light">Made by <a href="https://github.com/remtaine" className="underline hover:font-bold duration-500">Ben</a></span>
     </header>
   )
 }
