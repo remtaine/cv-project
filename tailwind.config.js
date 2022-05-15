2f6ad11c9806bd3045bf455c5f5f56247fb1ba86
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-15 16:50:00 +0800

    Add more screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      '3xs': '375px',
+      '2xs': '450px',
+      'xs': '560px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
